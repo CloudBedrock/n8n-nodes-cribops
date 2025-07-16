4a98a13fbc578712c5357d31aa730b9bbf568bcb
Jim Hankins <jhankins@cloudbedrock.com>
2025-07-15 23:37:13 -0400

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,7 @@
 const gulp = require('gulp');
 
 gulp.task('build:icons', function() {
-  // Copy icons to nodes directory
-  gulp.src('nodes/**/*.{png,svg}')
+  // Copy icons to nodes directory only
+  return gulp.src('nodes/**/*.{png,svg}')
     .pipe(gulp.dest('dist/nodes'));
-    
-  // Also copy the main cribops icon to credentials directory
-  return gulp.src('nodes/Cribops/cribops.svg')
-    .pipe(gulp.dest('dist/credentials'));
 });
