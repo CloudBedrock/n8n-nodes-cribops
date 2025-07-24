89239157961bef5f980384232f908e322cdd1067
Jim Hankins <jhankins@cloudbedrock.com>
2025-07-24 12:08:10 -0400

    update credentials requirements

--- credentials/CribopsApi.credentials.ts
+++ credentials/CribopsApi.credentials.ts
@@ -3,15 +3,12 @@ import {
   ICredentialTestRequest,
   ICredentialType,
   INodeProperties,
-  Icon,
 } from 'n8n-workflow';
 
 export class CribopsApi implements ICredentialType {
   name = 'cribopsApi';
   displayName = 'Cribops API';
-  documentationUrl = 'https://github.com/CloudBedrock/n8n-nodes-cribops/wiki';
-  // Icon removed due to n8n bug with credential icons in community nodes
-  // See: https://github.com/n8n-io/n8n/issues/11642
+  documentationUrl = 'https://docs.cribops.com';
   properties: INodeProperties[] = [
     {
       displayName: 'API Token',
@@ -31,30 +28,6 @@ export class CribopsApi implements ICredentialType {
       default: 'https://api.cribops.com',
       required: true,
       description: 'Base URL of the Cribops API',
-    },
-    {
-      displayName: 'Account ID',
-      name: 'accountId',
-      type: 'string',
-      default: '',
-      description: 'Account ID for cloud service integration',
-    },
-    {
-      displayName: 'Account Secret',
-      name: 'accountSecret',
-      type: 'string',
-      typeOptions: {
-        password: true,
-      },
-      default: '',
-      description: 'Account secret key for cloud service integration',
-    },
-    {
-      displayName: 'Region',
-      name: 'region',
-      type: 'string',
-      default: 'us-east-1',
-      description: 'Region for cloud service integration',
     },
   ];
 
